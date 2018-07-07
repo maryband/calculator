750d753ef7d2c9bb71a7c18dddb62e014ec566ad
maryband <40365394+maryband@users.noreply.github.com>
2018-07-07 19:27:04 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,75 +1,15 @@
-document.querySelector('.one').addEventListener('click', clickOne);
-document.querySelector('.two').addEventListener('click', clickTwo);
-document.querySelector('.three').addEventListener('click', clickThree);
-document.querySelector('.four').addEventListener('click', clickFour);
-document.querySelector('.five').addEventListener('click', clickFive);
-document.querySelector('.six').addEventListener('click', clickSix);
-document.querySelector('.seven').addEventListener('click', clickSeven);
-document.querySelector('.eight').addEventListener('click', clickEight);
-document.querySelector('.nine').addEventListener('click', clickNine);
-document.querySelector('.zero').addEventListener('click', clickZero);
-document.querySelector('.addition').addEventListener('click', addition);
-document.querySelector('.substraction').addEventListener('click', substraction)
-document.querySelector('.calc').addEventListener('click', calc);
-document.querySelector('.multiplication').addEventListener('click', multiplication);
-document.querySelector('.division').addEventListener('click', division);
+document.querySelectorAll('.digit').forEach(el=>el.addEventListener('click', digitClicked));
+document.querySelectorAll('.operation').forEach(el=>el.addEventListener('click', operationClicked));
 document.querySelector('.clear').addEventListener('click', clear);
+document.querySelector('.calc').addEventListener('click', calc);
 const display = document.querySelector('.display');
 
-function clickOne() {
-    display.value += 1;
+function digitClicked() {
+    display.value += event.target.innerText;
 }
 
-function clickTwo() {
-    display.value += 2;
-}
-
-function clickThree() {
-    display.value += 3;
-}
-
-function clickFour() {
-    display.value += 4;
-}
-
-function clickFive() {
-    display.value += 5;
-}
-
-function clickSix() {
-    display.value += 6;
-}
-
-function clickSeven() {
-    display.value += 7;
-}
-
-function clickEight() {
-    display.value += 8;
-}
-
-function clickNine() {
-    display.value += 9;
-}
-
-function clickZero() {
-    display.value += 0;
-}
-
-function addition() {
-    display.value += '+';
-}
-
-function substraction() {
-    display.value += '-';
-}
-
-function multiplication() {
-    display.value += '*';
-}
-
-function division() {
-    display.value += '/';
+function operationClicked() {
+    display.value += event.target.innerText;
 }
 
 function calc() {
